66a4e1ff877e8f2c0cc78e44994dbfea75ff1945
Ailincai, Andrei-Cristian  WI/CBR-ETTD <ailinadr@schaeffler.com>
2026-03-24 12:54:17 +0200

    Updated ThirdParty

--- cmake/fetch_cli11.cmake
+++ cmake/fetch_cli11.cmake
@@ -1,6 +1,6 @@
 FetchContent_Declare(
   cli11
-  URL https://github.com/CLIUtils/CLI11/archive/refs/tags/v2.5.0.tar.gz
+  URL https://github.com/CLIUtils/CLI11/archive/refs/tags/v2.6.2.tar.gz
 )
 
 FetchContent_MakeAvailable(cli11)
--- cmake/fetch_doctest.cmake
+++ cmake/fetch_doctest.cmake
@@ -1,7 +1,7 @@
 FetchContent_Declare(
   doctest
   GIT_REPOSITORY https://github.com/doctest/doctest
-  GIT_TAG v2.4.11
+  GIT_TAG v2.4.12
 )
 
 FetchContent_GetProperties(doctest)
